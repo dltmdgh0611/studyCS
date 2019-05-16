045e6c80ab975bbb7fd99216cad272df9eb13091
Lee_seung_ho <38284097+dltmdgh0611@users.noreply.github.com>
2019-05-16 20:56:33 +0900

    0

---
+++ 7.class/struct.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyCS.structure
+{
+    struct Point3D
+    {
+        public int x, y, z;
+
+        public Point3D(int X, int Y, int Z)
+        {
+            this.x = X;
+            this.y = Y;
+            this.z = Z;
+        }
+        public override string ToString()
+        {
+            return string.Format($"{x},{y},{z}");
+        }
+    }
+    class structure_class
+    {
+        static void Main(string[] args)
+        {
+            Point3D p1;
+            p1.x = 10;
+            p1.y = 20;
+            p1.z = 30;
+
+            Console.WriteLine(p1.ToString());
+
+            Point3D p2 = new Point3D(100, 200, 300);
+            Point3D p3 = p2;
+            p3.z = 400;
+
+            Console.WriteLine(p2.ToString());
+            Console.WriteLine(p3.ToString());
+        }
+    }
+}
---
+++ 8.interface_/interface1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace studyCS._8.interface_
+{
+    interface ILogger
+    {
+        void WriteLog(string message);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine("{0} {1}", DateTime.Now.ToLocalTime(), message);
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        private StreamWriter writer;
+
+        public FileLogger(string path)
+        {
+            writer = File.CreateText(path);
+            writer.AutoFlush = true;
+        }
+
+        public void WriteLog(string message)
+        {
+            writer.WriteLine("{0} {1}", DateTime.Now.ToShortTimeString(), message);
+        }
+    }
+
+    class ClimateMonitor
+    {
+        private ILogger logger;
+        public ClimateMonitor(ILogger logger)
+        {
+            this.logger = logger;
+        }
+        public void start()
+        {
+            while(true)
+            {
+                Console.Write("온도 입력해 : ");
+                string temp = Console.ReadLine();
+                if (temp == "") break;
+
+                if(Convert.ToInt32(temp) <= 10) logger.WriteLog("쯤 온도는 " + temp + "도의 추운 날씨였다.");
+                else if(Convert.ToInt32(temp) <= 20) logger.WriteLog("쯤 온도는 " + temp + "도의 따스한 날씨였다.");
+                else logger.WriteLog("쯤 온도는 " + temp + "도의 더운 날씨였다.");
+            }
+        }
+    }
+
+    class interface_main
+    {
+        static void Main(string[] args)
+        {
+            ClimateMonitor Monitor = new ClimateMonitor(new FileLogger("MyLog.txt"));
+            Monitor.start();
+        }
+    }
+}
